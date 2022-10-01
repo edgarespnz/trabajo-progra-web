cd969503a8d9cc48b8851eb145ca448f3301452f
Luis <20173775@aloe.ulima.edu.pe>
2022-10-01 18:07:37 -0500

    add: navigate mockups

--- src/index.js
+++ src/index.js
@@ -3,14 +3,23 @@ import ReactDOM from 'react-dom/client';
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import Mockup1 from './Components/Mockup-1/Mockup1';
+import Mockup2 from './Components/Mockup-2/Mockup2';
 import './index.css';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header/>
-    <Mockup1/>
-    <Footer/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/mockup-1' element={<Mockup1/>} />
+        <Route path='/mockup-2' element={<Mockup2/>}/>
+        
+      </Routes>
+    </BrowserRouter>
     
   </React.StrictMode>
 );
